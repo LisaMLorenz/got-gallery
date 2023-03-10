062cfe793e3e85b17965a95982b585da77153eb6
LisaMLorenz <hej@frau-lorenz.de>
2023-03-10 19:27:55 +0000

    dan's code

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,22 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import './App.css';
+import CharacterGallery from './components/CharacterGallery'
 
-const Character = ({ family, fullName, imageUrl, title }) => {
-    return (
-        <div data-test='character-card' className="card-container">
-            <img data-test='character-image' src={imageUrl} alt={fullName} />
-            <div data-test='character-body' className="card-body">
-                <h1 data-test='character-content'>{title} {fullName} of {family}</h1>
-            </div>
-        </div>
-    );
-};
-
-Character.propTypes = {
-    family: PropTypes.string,
-    fullName: PropTypes.string,
-    imageUrl: PropTypes.string,
-    title: PropTypes.string,
+function App() {
+  return (
+    <div className="App" data-test="component-app">
+      <CharacterGallery />
+    </div>
+  );
 }
 
-export default Character;
+export default App;
